2922afc45f463b88c1f375a9428bd18bc1c44351
Bassem <Bsaleh103@gmail.com>
2020-04-13 20:58:04 +0200

    Push messages to firebase

---
+++ app/src/main/java/com/bassemsaleh/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,22 @@
+package com.bassemsaleh.flashchatnewfirebase;
+
+public class InstantMessage {
+    String message;
+    String author;
+
+    public InstantMessage() {
+    }
+
+    public InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
